09c35a299b13ee1e74b8322eb3748388ef7f35db
Martin Artnik <martin.artnik@gmail.com>
2020-10-27 19:10:31 +0100

    WIP: Basic window management

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -3,6 +3,7 @@
     <div ref="windowsContainer" id="windows">
       <component
         v-for="windowProps in windows"
+        :top="windowProps.top"
         :is="windowClass"
         :key="windowProps.windowId"
         :x="windowProps.x"
@@ -11,10 +12,13 @@
         :width="windowProps.width"
         :height="windowProps.height"
         :windowId="windowProps.windowId"
+        :maximized="windowProps.maximized"
+        @move="moveWindow"
         @close="closeWindow"
+        @maximize="maximizeWindow"
+        @clickWindow="clickWindow"
       />
     </div>
-    <button @click="createWindow">Create Window</button>
   </div>
 </template>
 
@@ -30,25 +34,43 @@ export default {
       windows: []
     };
   },
-  created() {
-    this.$on("close", windowUid => {
-      alert(windowUid);
-    });
+  created() {},
+  mounted() {
+    for (let i = 0; i < 3; i++) {
+      this.createWindow();
+    }
   },
   methods: {
+    findWindow(windowId) {
+      return this.windows.find(w => w.windowId == windowId);
+    },
     createWindow() {
       const windowProps = {
         title: "test",
-        x: Math.random() * 1000,
-        y: Math.random() * 500,
-        width: 200,
-        height: 100,
-        windowId: Math.random() * 99999999999
+        x: Math.round(Math.random() * 1000),
+        y: Math.round(Math.random() * 500),
+        width: 300,
+        height: 300,
+        windowId: Math.random() * 99999999999,
+        top: true,
+        maximized: false
       };
       this.windows.push(windowProps);
     },
-    closeWindow(windowId) {
-      this.windows = this.windows.filter(w => w.windowId != windowId);
+    maximizeWindow(data) {
+      const currentWindow = this.findWindow(data.id);
+      currentWindow.maximized = !currentWindow.maximized;
+    },
+    closeWindow(data) {
+      this.windows = this.windows.filter(w => w.windowId != data.id);
+    },
+    moveWindow(data) {
+      const currentWindow = this.findWindow(data.id);
+      currentWindow.x = data.x;
+      currentWindow.y = data.y;
+    },
+    clickWindow(data) {
+      this.windows.forEach(w => (w.top = w.windowId == data.id));
     }
   }
 };
@@ -58,15 +80,19 @@ export default {
 #desktop {
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 }
 
 #windows {
   position: relative;
   width: 100%;
   height: 100%;
-
-  .window {
-    position: absolute;
-  }
 }
 </style>
--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,22 +1,11 @@
 <template>
   <div
     class="window"
-    :style="
-      'top: ' +
-        y +
-        'px; left: ' +
-        x +
-        'px; width: ' +
-        width +
-        'px; height: ' +
-        height +
-        'px'
-    "
+    :style="layoutStyle"
+    @mousedown="$emit('clickWindow', { id: windowId })"
   >
-    <div class="title-bar">
-      <div class="title-bar-text">
-        {{ title }}
-      </div>
+    <div class="title-bar" @mousedown="grabWindow">
+      <div class="title-bar-text">{{ title }}</div>
 
       <div class="title-bar-controls">
         <button aria-label="Minimize" @click="minimize"></button>
@@ -25,7 +14,11 @@
       </div>
     </div>
     <div class="window-body">
-      <p>Hello, world!</p>
+      <pre>
+        {{ x }}
+        {{ y }}
+        {{ maximized }}
+      </pre>
       <section class="field-row" style="justify-content: flex-end">
         <button>OK</button>
         <button>Cancel</button>
@@ -43,20 +36,69 @@ export default {
     y: { type: Number, required: true },
     width: { type: Number, required: true },
     height: { type: Number, required: true },
-    windowId: { type: Number, required: true }
+    windowId: { type: Number, required: true },
+    top: { type: Boolean, required: true },
+    maximized: { type: Boolean, required: true }
+  },
+  data() {
+    return {
+      grabbed: false,
+      grabX: null,
+      grabY: null,
+      startX: null,
+      startY: null
+    };
+  },
+  computed: {
+    layoutStyle() {
+      let style = `left: ${this.maximized ? 0 : this.x}px; top: ${
+        this.maximized ? 0 : this.y
+      }px; z-index: ${this.top ? 1000 : 10};`;
+      style += this.maximized
+        ? "width: 100%; height: 100%"
+        : `width: ${this.width}px; height: ${this.height}px;`;
+      return style;
+    }
   },
   methods: {
     minimize() {
-      this.$emit("minimize", this.windowId);
+      this.$emit("minimize", { id: this.windowId });
     },
     maximize() {
-      this.$emit("maximize", this.windowId);
+      this.$emit("maximize", { id: this.windowId });
     },
     close() {
-      this.$emit("close", this.windowId);
+      this.$emit("close", { id: this.windowId });
+    },
+    grabWindow(e) {
+      this.grabbed = true;
+      this.grabX = e.clientX;
+      this.grabY = e.clientY;
+      this.startX = this.x;
+      this.startY = this.y;
+
+      document.addEventListener("mousemove", this.moveWindow);
+
+      document.addEventListener("mouseup", () => {
+        this.grabbed = false;
+        document.removeEventListener("mousemove", this.moveWindow);
+      });
+    },
+    moveWindow(e) {
+      if (!this.grabbed) return;
+
+      this.$emit("move", {
+        id: this.windowId,
+        x: Math.round(this.startX + (e.clientX - this.grabX)),
+        y: Math.round(this.startY + (e.clientY - this.grabY))
+      });
     }
   }
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.window {
+  position: absolute;
+}
+</style>
